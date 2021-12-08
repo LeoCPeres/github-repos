ba0e0bcea8274aa8ea8ab7d541c3b6a74a2cdb72
Leonardo Csik Peres <leonardo.peres@coreon.com.br>
2021-12-08 19:49:18 -0300

    Comunicação com API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,169 @@
-import React from "react";
+import { useState, useEffect, FormEvent, useRef } from "react";
 import { Card } from "./components/Card";
 import "./GlobalStyles.css";
 import "./Home.css";
+import { api } from "./services/api";
+import toast, { Toaster } from "react-hot-toast";
+
+interface SearchResultProps {
+  total_count: number;
+  incomplete_results: boolean;
+  items: [
+    {
+      id: number;
+      node_id: string;
+      name: string;
+      full_name: string;
+      private: boolean;
+      owner: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: false;
+      };
+      html_url: string;
+
+      description: null;
+      fork: false;
+      url: string;
+      forks_url: string;
+      keys_url: string;
+      collaborators_url: string;
+      teams_url: string;
+      hooks_url: string;
+      issue_events_url: string;
+      events_url: string;
+      assignees_url: string;
+      branches_url: string;
+      tags_url: string;
+      blobs_url: string;
+      git_tags_url: string;
+      git_refs_url: string;
+      trees_url: string;
+      statuses_url: string;
+      languages_url: string;
+      stargazers_url: string;
+      contributors_url: string;
+      subscribers_url: string;
+      subscription_url: string;
+      commits_url: string;
+      git_commits_url: string;
+      comments_url: string;
+      issue_comment_url: string;
+      contents_url: string;
+      compare_url: string;
+      merges_url: string;
+      archive_url: string;
+      downloads_url: string;
+      issues_url: string;
+      pulls_url: string;
+      milestones_url: string;
+      notifications_url: string;
+      labels_url: string;
+      releases_url: string;
+      deployments_url: string;
+      created_at: string;
+      updated_at: string;
+      pushed_at: string;
+      git_url: string;
+      ssh_url: string;
+      clone_url: string;
+      svn_url: string;
+
+      homepage: null;
+      size: number;
+      stargazers_count: number;
+      watchers_count: number;
+      language: string;
+      has_issues: boolean;
+      has_projects: boolean;
+      has_downloads: boolean;
+      has_wiki: boolean;
+      has_pages: boolean;
+      forks_count: number;
+
+      mirror_url: null;
+      archived: boolean;
+      disabled: boolean;
+      open_issues_count: number;
+
+      license: null;
+      allow_forking: boolean;
+      is_template: boolean;
+
+      topics: [];
+      visibility: string;
+      forks: number;
+      open_issues: number;
+      watchers: number;
+      default_branch: string;
+      score: number;
+    }
+  ];
+}
 
 function App() {
+  const [searchResult, setSearchResult] = useState<SearchResultProps>(
+    {} as SearchResultProps
+  );
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    api
+      .get(`search/repositories?q=facebook`)
+      .then((response) => setSearchResult(response.data));
+  }, [searchInput]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (inputRef.current?.value !== undefined) {
+      setSearchInput(inputRef.current.value);
+    }
+  }
+
+  console.log(searchResult);
   return (
     <div className="container">
+      <Toaster />
       <img src="../images/github.svg" alt="" className="img-github" />
       <header>
         <img src="./images/logo-completa.png" alt="" />
         <h1 className="title">Explore repositórios no Github.</h1>
 
-        <div className="search">
-          <input type="text" placeholder="Digite aqui" />
-          <button>Pesquisar</button>
-        </div>
+        <form className="search">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Digite aqui"
+            ref={inputRef}
+          />
+          <button type="submit" onClick={handleSearch}>
+            Pesquisar
+          </button>
+        </form>
       </header>
       <main>
-        <Card />
+        {searchResult.items.map((item) => {
+          return <p>{item.name}</p>;
+        })}
       </main>
     </div>
   );
